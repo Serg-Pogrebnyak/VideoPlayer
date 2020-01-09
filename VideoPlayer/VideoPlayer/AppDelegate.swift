45a9a95349d211322b718cd14b87ca0ecae9af3e
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-01-09 11:31:34 +0200

    clean project ready

---
+++ VideoPlayer/VideoPlayer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  IPhoneApp
+//
+//  Created by Sergey Pohrebnuak on 26.10.2019.
+//  Copyright © 2019 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
