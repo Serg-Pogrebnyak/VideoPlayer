92aee5925eab9c8d5efab3f070f7402611088962
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-01-09 10:32:00 +0200

    empty project

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Sergey Pohrebnuak on 09.01.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
