bcfd52ef4ca64dd9d5972292b3f20a6c9c6286fd
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2020-01-31 10:40:03 +0200

    added delete action for table view

--- VideoPlayer/VideoPlayer/Resources/Table View delegate and data source/CustomTableViewDelegate.swift
+++ VideoPlayer/VideoPlayer/Resources/Table View delegate and data source/CustomTableViewDelegate.swift
@@ -20,4 +20,14 @@ class CustomTableViewDelegate: NSObject, UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
         musicOrVideoArrayProtocol.startPlay(atIndex: indexPath.row, autoPlay: true)
     }
+
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "") { (action, indexPath) in
+            print("work method for ios 9")
+        }
+        deleteAction.backgroundColor = UIColor(patternImage: UIImage(named: "trash")!)
+
+        return [deleteAction]
+    }
 }
