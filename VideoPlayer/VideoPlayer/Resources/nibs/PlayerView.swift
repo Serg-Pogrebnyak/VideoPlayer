8cc0ddcb624c4464e65676545ca007f5cef8b713
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2020-02-11 13:18:14 +0200

    fixed bug with player view and rotation

--- VideoPlayer/VideoPlayer/Resources/nibs/PlayerView.swift
+++ VideoPlayer/VideoPlayer/Resources/nibs/PlayerView.swift
@@ -78,6 +78,9 @@ class PlayerView: UIView {
         super.layoutSubviews()
         gradientLayer?.frame = self.bounds
         gradientLayer?.roundCorners(corners: [.topLeft, .topRight], radius: 20)
+        if #available(iOS 10.0, *) {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
     }
 
     func finishedAnimation() {
